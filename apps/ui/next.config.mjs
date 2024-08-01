7ad0432f500835089f5ece34704ebc149faba7c9
Johny Rokita <jan@rokita.me>
2024-08-01 22:28:03 +0200

    Added a version display to the frontend.

--- apps/ui/next.config.mjs
+++ apps/ui/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_CHIEF_VERSION: process.env.NEXT_PUBLIC_CHIEF_VERSION,
+  },
 };
 
 export default nextConfig;
